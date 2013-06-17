abb83adf13fa522f27a6264ff2d9f8c45792dd43
David Cok <davidcok@users.noreply.github.com>
2013-06-17 01:40:00 +0000

    - fixed scoping of quantifiers in invariants and method specs (for esc) - added some new tests

--- src/openjml/verifythis/Customer.java
+++ src/openjml/verifythis/Customer.java
@@ -7,28 +7,47 @@ class Customer {
     //@ static invariant max >= 0;
 
     // only one customer in crit section
+    /* @ static invariant
+    @   (\forall Customer c1; \created(c1);
+          (\forall Customer c2; \created(c2);
+    @      c1.phase == 2 && c2.phase == 2 ==> c1 == c2));
+    @*/
+
     /*@ static invariant
-      @   (\forall Customer c1; \created(c1);
-            (\forall Customer c2; \created(c2);
-      @      c1.phase == 2 && c2.phase == 2 ==> c1 == c2));
+    @   (\forall Customer c1; ;
+          (\forall Customer c2; ;
+    @      c1.phase == 2 && c2.phase == 2 ==> c1 == c2));
+    @*/
+
+    // customer in crit section is minimal
+    /* @ static invariant
+      @   (\forall Customer c1; \created(c1) && c1.phase==2;
+      @      (\forall Customer c2; \created(c2) && c1!=c2 && c2.phase >= 1; 
+      @         c2.ticket > c1.ticket));
       @*/
 
     // customer in crit section is minimal
     /*@ static invariant
-      @   (\forall Customer c1; \created(c1) && c1.phase==2;
-      @      (\forall Customer c2; \created(c2) && c1!=c2 && c2.phase >= 1; 
+      @   (\forall Customer c1; c1.phase==2;
+      @      (\forall Customer c2; c1!=c2 && c2.phase >= 1; 
       @         c2.ticket > c1.ticket));
       @*/
 
     //@ invariant phase >= 0 && phase <= 2;
     //@ invariant ticket >= 0 && ticket <= max;
 
+    /* @
+    @ requires phase == 1 && 
+    @   (\forall Customer c; \created(c) && c!=this && c.phase >= 1; 
+    @       c.ticket > ticket);
+    @ ensures phase == 2;
+    @*/
     /*@
-      @ requires phase == 1 && 
-      @   (\forall Customer c; \created(c) && c!=this && c.phase >= 1; 
-      @       c.ticket > ticket);
-      @ ensures phase == 2;
-      @*/
+    @ requires phase == 1 && 
+    @   (\forall Customer c;  c!=this && c.phase >= 1; 
+    @       c.ticket > ticket);
+    @ ensures phase == 2;
+    @*/
     void enter() {
 	phase = 2;
     }
