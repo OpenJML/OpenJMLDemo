1096e98f4819b3f7e7d23a284bc3a0c2281b5acd
David Cok <davidcok@users.noreply.github.com>
2013-09-09 00:56:00 +0000

    Fixed esc problems with Taxpayer and BeanCan examples

---
+++ src/openjml/demo/Taxpayer.java
@@ -0,0 +1,147 @@
+//package sv_esc_solutions;
+
+
+/* 
+  This assignment illustrates how specifications (esp invariants and 
+  preconditions)  written in a formal language can help in removing 
+  errors in code. 
+
+  The assignment concerns a class "Taxpayer" that is used for taxpayers.
+
+ */
+class Taxpayer {
+
+ /* Part 1 of exercise. */
+	
+ //@ public invariant isFemale <==> !isMale;
+ //@ public invariant (isFemale && isMarried) ==> (spouse != null && spouse.isMale);
+ //@ public invariant (isMale   && isMarried) ==> (spouse != null && spouse.isFemale);
+ //@ public invariant isMarried ==> (spouse != null && spouse.spouse == this && spouse.isMarried);
+
+ /* Part 2 & 3 of exercise. */
+	
+ //@ public invariant (!isMarried && age < 65) ==> tax_allowance == DEFAULT_ALLOWANCE;
+ //@ public invariant (!isMarried && age >= 65) ==> tax_allowance == ALLOWANCE_OAP;
+ /*@ public invariant this.isMarried ==> 
+   @		   ((tax_allowance + spouse.tax_allowance == 2 * DEFAULT_ALLOWANCE <==> (age < 65  && spouse.age < 65))  ||
+   @			(tax_allowance + spouse.tax_allowance == 2 * ALLOWANCE_OAP	 <==> (age >= 65 && spouse.age >= 65)) ||
+   @			(tax_allowance + spouse.tax_allowance == DEFAULT_ALLOWANCE + ALLOWANCE_OAP));
+   @*/
+	
+ /* FIELDS */
+
+ /* isFemale is true iff the person is female */
+ /*@ spec_public @*/ boolean isFemale;
+
+ /* isMale is true iff the person is male */
+ /*@ spec_public @*/ boolean isMale;
+
+ Taxpayer father, mother; // These fields won't really be used until
+                          // the next part of the exercise.
+
+ /* Age in years */ 
+ /*@ spec_public @*/ int age; 
+
+ /*@ spec_public @*/ boolean isMarried; 
+
+ /* Reference to spouce if person is married, null otherwise */
+ /*@ spec_public nullable @*/ Taxpayer spouse;
+
+ /* Constant default income tax allowance (belastingvrije som) */
+ /*@ spec_public @*/ static final int DEFAULT_ALLOWANCE = 5000;
+
+ /* Constant income tax allowance for Old Age Pensioners over 65 */
+ /*@ spec_public @*/ static final  int ALLOWANCE_OAP = 7000;
+
+ /* Income tax allowance (belastingvrije som) */
+ /*@ spec_public @*/ int tax_allowance; 
+
+ /* Income per year */
+ int income; 
+
+ /* CONSTRUCTOR */
+
+ Taxpayer(boolean jongetje, Taxpayer ma, Taxpayer pa) {
+   age = 0;
+   isMarried = false;
+   this.isMale = jongetje;
+   this.isFemale = !jongetje;
+   mother = ma;
+   father = pa;
+   spouse = null;
+   income = 0;
+   tax_allowance = DEFAULT_ALLOWANCE;
+   /* The line below makes explicit the assumption that a new Taxpayer is not 
+    * married to anyone yet. A bit silly of course, but we need this to keep 
+    * ESC happy. */
+   //@ assume (\forall Taxpayer p; p.spouse != this); 
+ } 
+
+ /* METHODS */
+
+ /* Marry to new_spouse */
+ //@ requires !isMarried && !new_spouse.isMarried;
+ //@ requires isFemale ==> new_spouse.isMale;
+ //@ requires isMale ==> new_spouse.isFemale;
+ void marry(Taxpayer new_spouse) {
+  spouse = new_spouse;
+  isMarried = true;
+  new_spouse.spouse = this;		//-------------------------------------------> ADDED (Part 1)
+  new_spouse.isMarried = true;	//-------------------------------------------> ADDED (Part 1)
+ }
+ 
+ /* Divorce from current spouse */
+ //@ requires isMarried;
+ void divorce() {
+  if (this.age < 65) this.tax_allowance = DEFAULT_ALLOWANCE;		//-------> ADDED (Part 2 & 3)
+  else this.tax_allowance = ALLOWANCE_OAP;							//-------> ADDED (Part 2 & 3)
+  if (spouse.age < 65) spouse.tax_allowance = DEFAULT_ALLOWANCE;	//-------> ADDED (Part 2 & 3)
+  else spouse.tax_allowance = ALLOWANCE_OAP;						//-------> ADDED (Part 2 & 3)
+  //@ assert spouse != null;
+  spouse.spouse = null;
+  spouse.isMarried = false;		//-------------------------------------------> ADDED (Part 1)
+  spouse = null;
+  isMarried = false;
+ }
+
+ /* Transfer change of the tax allowance from this person to his/her spouse */
+ //@ requires this.isMarried;
+ void transferAllowance(int change) {
+  tax_allowance = tax_allowance - change;
+  spouse.tax_allowance = spouse.tax_allowance + change;
+ }
+
+ /* Taxpayer has a birthday and the age increases by one */
+ /*@ requires !isMarried ==>
+   @		  (tax_allowance == DEFAULT_ALLOWANCE || tax_allowance == ALLOWANCE_OAP);
+   @*/
+ /*@ requires isMarried ==>
+   @		  (tax_allowance == 0 || tax_allowance == DEFAULT_ALLOWANCE ||
+   @		   tax_allowance == ALLOWANCE_OAP || tax_allowance == 2 * DEFAULT_ALLOWANCE ||
+   @		   tax_allowance == 2 * ALLOWANCE_OAP || tax_allowance == DEFAULT_ALLOWANCE + ALLOWANCE_OAP);
+  */
+ /*@ requires isMarried ==>
+   @		  (spouse.tax_allowance == 0 || spouse.tax_allowance == DEFAULT_ALLOWANCE ||
+   @		   spouse.tax_allowance == ALLOWANCE_OAP || spouse.tax_allowance == 2 * DEFAULT_ALLOWANCE ||
+   @		   spouse.tax_allowance == 2 * ALLOWANCE_OAP || spouse.tax_allowance == DEFAULT_ALLOWANCE + ALLOWANCE_OAP);
+   @*/
+ void haveBirthday() {
+  age++;
+  
+  //Below: ADDED (Part 3)
+  if (this.age == 65) {
+	  if (this.isMarried && this.spouse != null) {
+		  if (this.tax_allowance == 0) {
+			  this.spouse.tax_allowance = this.spouse.tax_allowance + (ALLOWANCE_OAP - DEFAULT_ALLOWANCE);
+		  } else if (this.spouse.tax_allowance == 0) {
+			  this.tax_allowance = this.tax_allowance + (ALLOWANCE_OAP - DEFAULT_ALLOWANCE);
+		  } else {
+			  this.tax_allowance = ALLOWANCE_OAP;
+		  }
+	  } else {
+		  this.tax_allowance = ALLOWANCE_OAP;
+	  }
+  }
+ }
+
+}
---
+++ src/openjml/demo/Time.java
@@ -0,0 +1,103 @@
+//package sv_rac_solutions;
+
+/**
+ * Copyright (c) 1999 GEMPLUS group. All Rights Reserved.
+ *------------------------------------------------------------------------------
+ *  Project name:  PACAP  - cas d'�tude -
+ *
+ *
+ *  Platform    :  Java virtual machine
+ *  Language    :  JAVA 1.1.x
+ *  Devl tool   :  Symantec VisualCafe
+ *
+ *  @author: Thierry Bressure
+ *  @version 1
+ *------------------------------------------------------------------------------
+ */
+
+/* model the time for the transactions*/
+public class Time extends Object{
+
+
+	//@ public model int time;
+	//@ represents time <- hour * 60 + minute;
+	
+	//@ public model int maxTime;
+	//@ represents maxTime <- 23 * 60 + 59;
+
+	/*@
+	 	public invariant time >= 0;
+	 	public invariant time <= maxTime;
+	 	
+	 	public invariant 0 <= minute && minute < 60;
+	 	public invariant 0 <= hour && hour < 24;
+	 	
+	 	public constraint time >= \old(time);
+	 */
+	
+	private /*@ spec_public @*/ byte hour;
+	private /*@ spec_public @*/ byte minute;
+
+	/*@
+	 	requires h >= getHour();
+	 	requires m >= getMinute();
+	 	requires 0 <= m && m < 60;
+	 	requires 0 <= h && h < 24;
+		ensures time == h * 60 + m;
+	 */
+	public void setTime(byte h, byte m) {
+		hour = h;
+		minute = m;
+	} 
+
+	/*@
+	  	requires h != null;
+	  	requires h.getHour() >= getHour();
+	  	requires h.getMinute() >= getMinute();
+	  	ensures this.time == h.time;
+	 */
+	public void setTime(Time h) {
+		setTime(h.getHour(), h.getMinute());
+	} 
+
+	/*@ 
+	 	ensures \result == (time - minute) / 60;
+	 */
+	public /*@ pure */ byte getHour(){
+		return hour;
+	}
+
+	/*@ 
+	 	ensures \result == time - (hour * 60);
+	 */
+	public /*@ pure */ byte getMinute(){
+		return minute;
+	}
+
+	/*@
+	 	requires bArray != null;
+	 	
+	 	requires offset >= 0;
+	 	requires offset < 32766; //prevents overflowing of offset/aux
+	 	
+	 	requires 0 <= offset && offset < bArray.length - 1;
+	 	requires bArray.length >= 2;
+	 	
+	 	ensures (\forall int i; 0 <= i && i < offset; bArray[i] == \old(bArray[i]));
+	 	ensures bArray[offset] == (time - minute) / 60;
+	 	ensures bArray[offset+1] == time - (hour * 60);
+	 */
+	public short  getTime(byte [] bArray, short offset){
+		short aux = offset;
+		bArray[aux++] = hour;
+		bArray[aux++] = minute;	
+		return (short) (offset + (short) 2);
+	}
+	
+	public static void main(String[] args) {
+		Time t = new Time();
+		t.setTime((byte)10, (byte)01);
+		t.getHour();
+		t.getMinute();
+	}
+}
