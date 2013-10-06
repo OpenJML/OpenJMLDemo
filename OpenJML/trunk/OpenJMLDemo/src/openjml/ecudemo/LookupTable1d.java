c033e8485e4d22caf7bf1ab280a2f923d043137f
davidcok <davidcok@1627bae4-3225-46cb-a19f-684862636fd9>
2013-10-06 21:34:53 +0000

    Fixed problems with invariant checking; added option to control timeout; added ECU demo

---
+++ OpenJML/trunk/OpenJMLDemo/src/openjml/ecudemo/LookupTable1d.java
@@ -0,0 +1,33 @@
+
+public class LookupTable1d {
+
+	LookupScale scaleHorizontal;
+	
+	int[] lookupValues; // does not have to be sorted
+	
+	//@ invariant lookupValues.length == scaleHorizontal.values.length;
+	
+    /*@ normal_behavior
+	  @ requires lookupValues.length > 1;
+	  @ requires scale.values.length == lookupValues.length;
+	  @ ensures this.scaleHorizontal == scale;
+	  @ ensures this.lookupValues == lookupValues;
+	  @ assignable this.lookupValues, this.scaleHorizontal;
+	  @*/
+	public LookupTable1d(LookupScale scale, int[] lookupValues) {
+		this.scaleHorizontal = scale;
+		this.lookupValues = lookupValues;
+	}
+	
+	public /*@ pure @*/ int getValue(SensorValue sv) {
+		ScaleIndex si = scaleHorizontal.lookupValue(sv);
+		int i = si.getIntPart();
+		int f = si.getFracPart();
+		int v = lookupValues[i];
+		if(i<lookupValues.length-1) {
+			int vn = lookupValues[i+1];
+			v = v + (vn - v)*f/100; // linear interpolate
+		}
+		return v;
+	}
+}
