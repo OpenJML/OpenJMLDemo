f4b85977cba60cd37e3989f4b69e42de1dbf4433
davidcok <davidcok@1627bae4-3225-46cb-a19f-684862636fd9>
2013-10-07 00:25:17 +0000

    Version 0.5.6

--- OpenJML/trunk/OpenJMLDemo/src/openjml/purseMod/CardTest.java
+++ OpenJML/trunk/OpenJMLDemo/src/openjml/purseMod/CardTest.java
@@ -8,7 +8,7 @@ public class CardTest {
 	public static void main(String[] args) {
 
 		// declare variables
-		int owner = 23;
+		int owner = 23; 
 		Terminal terminal = new Terminal(owner);
 		Card card = new Card();
 		
@@ -37,6 +37,7 @@ public class CardTest {
 		// rerun and continue
 		
 		// insert card in terminal
+		
 		terminal.insertCard(card);
 		
         // note there is an error in clearPin(), called in insertCard(), but this is not detected here,
@@ -54,7 +55,7 @@ public class CardTest {
 		for (int i = 0; i < 16; i++) {
 			try {terminal.load(i);} catch (Exception e) {};
 		} 
-
+		
 		// Error 3 detected
 		// JMLInternalNormalPostconditionError: by method Token.sign
 		// at purse.Token.sign(CardTest.java:1330)
@@ -69,7 +70,7 @@ public class CardTest {
 		
 		// make 17th load, loaded array should be expanded
 		try {terminal.load(24);} catch (Exception e) {};
-
+		
 		// Error 4 detected
 		// JMLInternalExceptionalPostconditionError: by method Card.append regarding specifications at
 		// File "..\..\..\workspace\Assignment 1\src\purse\CardTest.java", line 211, character 43, when
@@ -86,7 +87,7 @@ public class CardTest {
 		
 		// make payment
 		try {terminal.pay(26);} catch (Exception e) {};
-
+		
 		// Error 5 detected
 		
 		// JMLInternalNormalPostconditionError: by method Card.pay
