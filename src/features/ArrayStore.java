8ee73cfa5a16123456f96d97c5d6022cdfdf3c16
David Cok <davidcok@users.noreply.github.com>
2015-03-26 20:25:11 +0000

    Fixing up ArrayStore implementation and tests

--- src/features/ArrayStore.java
+++ src/features/ArrayStore.java
@@ -3,7 +3,8 @@ package features;
 public class ArrayStore {
 
 	public static void main(String ... args) {
-		test();
+		try { test(); } catch (ArrayStoreException e) {}
+		try { test2(); } catch (ArrayStoreException e) {}
 	}
 	
 	public static void test() {
@@ -13,6 +14,6 @@ public class ArrayStore {
 	
 	public static void test2() {
 		Object[] a = new Integer[10];
-		a[0] = true;  // Makes an internal exception
+		a[0] = true;  // Requires implicit conversion, which was a bug
 	}
 }
