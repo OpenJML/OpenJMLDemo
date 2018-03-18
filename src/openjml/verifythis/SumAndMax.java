783e4ba3a53287d3aef64889513a4f227191c843
davidcok <davidcok@github.com>
2018-03-18 09:33:26 +0100

    Updating example to use \count

--- src/openjml/verifythis/SumAndMax.java
+++ src/openjml/verifythis/SumAndMax.java
@@ -4,7 +4,7 @@ public class SumAndMax {
 	//@ ensures \result == (\sum int i; 0<=i && i < a.length; a[i]);
 	static public int sum(int[] a) {
 		int sum = 0;
-		//@ loop_invariant sum == (\sum int j; 0<=j && j<\index; a[j]);
+		//@ loop_invariant sum == (\sum int j; 0<=j && j<\count; a[j]);
 		// @ loop_modifies sum;
 		for (int i: a) {
 			sum += i;
@@ -18,7 +18,7 @@ public class SumAndMax {
 	//@ ensures (\exists int i; 0<=i && i < a.length; a[i] == \result);
 	static public int max(int[] a) {
 		int max = a[0];
-		//@ loop_invariant max == (\max int j; 0<=j && j<\index; a[j]);
+		//@ loop_invariant max == (\max int j; 0<=j && j<\count; a[j]);
 		// @ loop_modifies max;
 		for (int i: a) {
 			if (max < i) max = i;
