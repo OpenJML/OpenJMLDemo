e57a1a7b11b774629507d5075c4247b5ac59ff69
davidcok <davidcok@github.com>
2024-02-05 18:59:51 -0500

    Touch ups to demos

--- src/features/NegativeArraySize.java
+++ src/features/NegativeArraySize.java
@@ -2,14 +2,14 @@ package features;
 
 public class NegativeArraySize {
 
-	public static void main(String ... args) {
-		test(1);
-		test(0);
-		test(-1);
-	}
-	
-	public static int[] test(int size) {
-		int[] a = new int[size];
-		return a;
-	}
+    public static void main(String ... args) {
+        test(1);
+        test(0);
+        test(-1);
+    }
+
+    public static int[] test(int size) {
+        int[] a = new int[size];
+        return a;
+    }
 }
